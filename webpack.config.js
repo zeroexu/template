35c1694d4e95821b7f033d16591b8dd4d624c066
Ali <zero.exu@gmail.com>
2019-09-17 10:37:10 -0300

    Agregando configuraciones al webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.export = {
     entry: './src/index.js',
     output: {
         path: path.join(__dirname + 'dist'),
         filename: 'bundle.js'
-    }
+    },
+
+    modules: {
+        rules: []
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
 }
