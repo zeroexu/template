52a7823e87e4cfaf5e158ed183e166f600ee138a
Ali <zero.exu@gmail.com>
2019-09-17 22:50:15 -0300

    archivo de llaves

---
+++ src/utils/keys.js
@@ -0,0 +1,23 @@
+require('dotenv').config({
+    path: process.env.NODE_ENV
+        ? `${__dirname}/../../envs/.env.${process.env.NODE_ENV}`
+        : `${__dirname}/../../envs/.env.development`
+});
+
+module.exports = {
+    SETTINGS: {
+        PORT: process.env.PORT,
+        APP_NAME: process.env.APP_NAME,
+        NODE_ENV: process.env.NODE_ENV,
+        MAINTENANCE: process.env.MAINTENANCE,
+        LANGUAGE: process.env.LANGUAGE
+    },
+    REPOSITORIES: {
+        GRAPH_API: process.env.GRAPHQL_API
+    },
+    APP_KEYS: {
+        PORT: 'port',
+        ENV: 'env',
+        APP_NAME: 'app_name'
+    }
+};
