66d5da93a459a12ab000315195d7b145724fa7bd
Ali <zero.exu@gmail.com>
2019-09-18 20:14:48 -0300

    colocando data de prueba

--- src/containers/home.jsx
+++ src/containers/home.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import Header from '../components/header';
 import MiniCardList from '../components/mini_card_list';
 import CardList from '../components/card_list';
+import Photo from '../components/test/photo';
+import RedSquare from '../components/test/RedSquare';
+import { Context, initialState, reducer } from "../store/index";
+const path = require('path');
+const sampleData = [
+    {
+        label: 'Un test de Algo', price: '$777', src: '',
+    },
+    {
+        label: 'Un test de Algo', price: '$777', src: '',
+    },
+    {
+        label: 'Un test de Algo', price: '$777', src: '',
+    },
+    {
+        label: 'Un test de Algo', price: '$777', src: '',
+    },
+    {
+        label: 'Un test de Algo', price: '$777', src: '',
+    },
+];
+
 
 const Home = () => {
-    return <React.Fragment>
-        <header />
-        <MiniCardList />
-        <CardList />
-    </React.Fragment>
+    const [store, dispatch] = useReducer(reducer, initialState);
+    return <Context.Provider value={{ store, dispatch }}>
+        <React.Fragment>
+            <Header />
+            <MiniCardList miniCards={sampleData} />
+            <CardList cards={sampleData} />
+        </React.Fragment>
+    </Context.Provider>
 }
+
+
 
 export default Home;
