edd0a2f524a04e0ae573611d33d0c5840b394dc3
Ali <zero.exu@gmail.com>
2019-09-18 13:40:01 -0300

    refactorizando el utils Requester

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,36 @@
 const { request } = require('graphql-request');
+const Keys = require('./keys');
+const { allCards } = require('./queries');
+
 
 const requesterGraphql = (url, query) => {
     return request(url, query);
 }
 
+const requesterGraphqlFormatState = async (url, query) => {
+    const response = await requesterGraphql(url, query);
+    return {
+        loading: false,
+        data: response
+    }
+}
 
-module.exports = {
-    requesterGraphql
+const handleRequestCards = async (dispatch) => {
+    let url = Keys.REPOSITORIES.GRAPH_API;
+    let query = allCards();
+
+    const response = await requesterGraphql(url, query);
+
+
+    return dispatch({
+        type: "getAllCards",
+        response: {
+            loading: false,
+            data: response
+        }
+    });
+}
+
+export default {
+    requesterGraphql, requesterGraphqlFormatState, handleRequestCards
 };
