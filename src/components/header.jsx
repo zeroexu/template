eff99b736bdd34932b26e3a5d07814213d858158
Ali <zero.exu@gmail.com>
2019-09-19 22:34:38 -0300

    Corrigiendo titulos

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import image from '../assets/images/logox100Hider.jpg';
+import image from '../assets/images/logox100.jpg';
 const Header = () => {
     return <div className={'header'}>
         <img src={image} alt="" className={'float-left'} />
-        <h2 className={'float-left'}>un slogan para algo</h2>
+        <h2 className={'float-left'}>Viajar es la mejor guita invertida</h2>
         <div className={'cleaner-floater'}></div>
     </div>
 }
