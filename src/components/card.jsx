b4df6d233389208a7fdb10177e8a69f56cc34ead
Ali <zero.exu@gmail.com>
2019-09-20 14:48:03 -0300

    Hacer clickable toda la card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 
 const Card = ({ card }) => {
-  const { description, price, imgUrl } = card
+  const { description, price, imgUrl, link } = card
   return (
     <div className='card element'>
-      <img src={imgUrl} width={250} height={220} />
-      <div className='inner'>
-        <div>
-          <span className='sub-title float-left'>Directo</span>
-          <span className='sub-title float-right'>Precio desde</span>
+      <a href={link} target="_blamk">
+        <img src={imgUrl} width={250} height={220} />
+        <div className='inner'>
+          <div>
+            <span className='sub-title float-left'>Directo</span>
+            <span className='sub-title float-right'>Precio desde</span>
+          </div>
+          <div className='cleaner-floater'>
+            <span className='title float-left'>{description}</span>
+            <span className='title float-right'>$ {price}</span>
+          </div>
+          <div>
+            <button>Ver Vuelo</button>
+          </div>
+          <div className='cleaner-floater' />
         </div>
-        <div className='cleaner-floater'>
-          <span className='title float-left'>{description}</span>
-          <span className='title float-right'>$ {price}</span>
-
-        </div>
-        <div>
-          <button>Ver Vuelo</button>
-        </div>
-        <div className='cleaner-floater' />
-      </div>
+      </a>
     </div>)
 }
 
