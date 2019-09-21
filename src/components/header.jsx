d9d0a42aab8ed77a022764c685e8c4df437a2c61
Ali <zero.exu@gmail.com>
2019-09-21 12:00:54 -0300

    mejorando estilos

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import image from '../assets/images/logox100.jpg'
 const Header = () => {
   return (
-    <div className='header'>
-      <img src={image} alt='' className='float-left' />
-      <h2 className='float-left'>Viajar es la mejor guita invertida</h2>
-      <div className='cleaner-floater' />
-    </div>)
+    <div className='header-wrapper'>
+      <div className='header'>
+        <img src={image} alt='' className='float-left' />
+        <h2 className='float-left'>Viajar es la mejor guita invertida</h2>
+        <div className='cleaner-floater' />
+      </div>
+    </div>
+  )
 }
 
 export default Header
